a06e0a6933f21988d6889a8fa475b28d3fc21513
Almond-Latte <almondcaramellatte@gmail.com>
2023-10-13 17:11:28 +0900

    フォントの修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import { AppProps } from 'next/app'
 import '../styles/index.css'
 import 'zenn-content-css'
 import { useEffect } from 'react'
-import { Noto_Sans_JP } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
-const notojp = Noto_Sans_JP({
-  weight: ['400', '500'],
+const inter = Inter({
   subsets: ['latin'],
-  display: 'swap'
 })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -15,7 +13,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     import("zenn-embed-elements"); // 数式をブラウザでレンダリングできるように
   }, []);
   return (
-    <main className={notojp.className}>
+    <main className={inter.className}>
       <Component {...pageProps} />
     </main>
   )
