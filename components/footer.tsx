c5cd05dbcb616f67ab3610bc8355cca5c1708d33
Almond-Latte <147462539+Almond-Latte@users.noreply.github.com>
2023-10-31 00:33:39 +0900

    feature: フッターの作成 #45

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,13 @@
+import { getYear } from 'date-fns';
 import Container from './container';
 
 const Footer = () => {
+  const date = new Date();
   return (
     <footer className='bg-neutral-50 border-t border-neutral-200'>
       <Container>
-        <div className='py-28 flex flex-col lg:flex-row items-center'>
-          <h4 className='text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2'>
-            Statically Generated with Next.js.
-          </h4>
-          <div className='flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2'>
-            <a
-              href='https://nextjs.org/docs/basic-features/pages'
-              className='mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0'
-            >
-              Read Documentation
-            </a>
-            <a className='mx-3 font-bold hover:underline'>View on GitHub</a>
-          </div>
+        <div className='py-10 text-center text-zinc-500'>
+          <p>&copy; Copyright {getYear(date)} Almond Latte. All Rights Reserved.</p>
         </div>
       </Container>
     </footer>
