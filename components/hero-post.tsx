bbb7cdb31b14e7cac5166d58d762d4b99039830c
Almond-Latte <147462539+Almond-Latte@users.noreply.github.com>
2023-10-31 16:06:53 +0900

    fix: レスポンシブデザインの修正 #49
    スマホ用レスポンシブデザインの修正と、
    ついでに
    - ヘッダ画像の正確なサイズ指定
    - post-titleの画像の修正

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const HeroPost = ({ title, postDate, updateDate, slug }: Props) => {
   return (
-    <section>
+    <section className='px-5'>
       <div className='md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28'>
         <div>
           <h3 className='mb-4 text-4xl lg:text-5xl leading-tight'>
--- components/intro.tsx
+++ components/intro.tsx
@@ -2,10 +2,8 @@ import Image from 'next/image';
 const Intro = () => {
   return (
     <section className='flex flex-col items-center pt-20 mb-16 '>
-      <Image src='/favicon/logo_alpha.png' alt='Logo' width={400} height={100} />
-      <h4 className='text-center md:text-left text-lg mt-5 md:pl-8'>
-        いっぱんだいがくせいの技術ブログ
-      </h4>
+      <Image src='/favicon/logo_alpha.png' alt='Logo' width={400} height={116} />
+      <h4 className='text-center text-lg  md:pl-8'>いっぱんだいがくせいの技術ブログ</h4>
     </section>
   );
 };
