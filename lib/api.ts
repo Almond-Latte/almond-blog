92d83fdd036a4ab0ddf09e8a3773f3a40b96a019
Almond-Latte <147462539+Almond-Latte@users.noreply.github.com>
2024-05-08 11:46:07 +0900

    #78 投稿を公開日でソートする

--- lib/api.ts
+++ lib/api.ts
@@ -42,6 +42,6 @@ export function getAllPosts(fields: string[] = []) {
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+    .sort((post1, post2) => (post1.postDate > post2.postDate ? -1 : 1));
   return posts;
 }
